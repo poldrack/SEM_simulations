c836149c3977257ac4b200fda95735e428f8d42d
poldrack <poldrack@gmail.com>
2015-12-01 20:03:53 -0800

    added third model

--- SEM_sim2.R
+++ SEM_sim2.R
@@ -9,14 +9,13 @@ if (is.na(nsamp)) {nsamp=600}
 
 if (is.na(samp_prop)) {
    cat('no command line args given - using canned values\n')
-   samp_prop=0.2
+   samp_prop=1.0
    simnum=1
-   imp=1
+   imp=0
 }
 
 library(lavaan)
-library(gtools)
-library(Amelia)
+#library(gtools)
 
 c0=read.table('set1.csv',sep=',',header=TRUE,na.strings=c('NA','.'))
 
@@ -79,10 +78,25 @@ FG =~ choose_hi_prob_mn + avoid_low_prob_mn
 FH =~ asrs_score + hopkins_globalseverity + bis_factor1_ci + bis_factor2_bi + persistance + bart_meanadjustedpumps
 FI =~ bart_meanadjustedpumps + cpt_fa + smnm_maint_dprime + vmnm_maint_dprime 
 '
-
 fit2 <- cfa(cfa_model2, data =data2)
 
-runcfa = function(data,model,model2,impute=0,ameliaridge=0.05,missing='fiml',ridge=0.1,scale=TRUE) {
+cfa_model3='FA =~ bis_factor1_ci + bis_factor2_bi + dysfunc_total +  func_total + mpq_score + scorei + persistance + novelty
+FB =~ ssp_totalraw + ds_totalraw + mr_totalraw + voc_totalraw + lns_totalraw + vcap_dprime + vmnm_maint_dprime 
++ vmnm_manip_dprime
+FC =~ hopkins_globalseverity + func_total + scorev + bipollarii_sumscore + golden_sumscore + chaphypo_total 
++ persistance + novelty + harmavoidance
+FD =~ vr2dr_totalraw + ssp_totalraw + mr_totalraw + scap_dprime + sr_dprime + smnm_maint_dprime + smnm_manip_dprime
+FE =~ hopkins_globalseverity + bis_factor2_bi + scorei + bipollarii_sumscore + golden_sumscore + chaphypo_total 
++ persistance
+FF =~ chapinf_total + chapsoc_total + chapphy_total + reward_dependence
+FG =~ choose_hi_prob_mn + avoid_low_prob_mn
+FH =~ asrs_score + hopkins_globalseverity + bis_factor1_ci + bis_factor2_bi + persistance + bart_meanadjustedpumps + cpt_fa 
++ smnm_maint_dprime + vmnm_maint_dprime 
+'
+
+fit3 <- cfa(cfa_model3, data =data2)
+
+runcfa = function(data,model,model2,model3,impute=0,ameliaridge=0.05,missing='fiml',ridge=0.1,scale=TRUE) {
   out=tryCatch(
     {
       if (impute==1) {
@@ -94,11 +108,12 @@ runcfa = function(data,model,model2,impute=0,ameliaridge=0.05,missing='fiml',rid
       }
       f=cfa(model,data=data,ridge=ridge,missing=missing,zero.add=c(0.1,0.1))
       f2=cfa(model2,data=data,ridge=ridge,missing=missing,zero.add=c(0.1,0.1))
+      f3=cfa(model3,data=data,ridge=ridge,missing=missing,zero.add=c(0.1,0.1))
       meas=fitmeasures(f,fit.measures='all')
-      meas2=fitmeasures(f2,fit.measures='all')
       a=anova(f,f2)
+      a2=anova(f2,f3)
       
-      c(meas['rmsea'],meas['bic2'],meas2['bic2'],a$"Pr(>Chisq)"[2])
+      c(meas['rmsea'],a$"Pr(>Chisq)"[2],a2$"Pr(>Chisq)"[2])
     },
     error=function(cond) {
     			 cat('something went wrong\n')
@@ -130,7 +145,7 @@ runcfa = function(data,model,model2,impute=0,ameliaridge=0.05,missing='fiml',rid
       }
     }
     # change impute to FALSE to see effects of no imputation
-    cfaout=runcfa(dsim,cfa_model,cfa_model2,impute=imp)
+    cfaout=runcfa(dsim,cfa_model,cfa_model2,cfa_model3,impute=imp)
 
 output=c(samp_prop,simnum,cfaout)
 if (imp==1) {outdir='outputs_amelia'} else {outdir='outputs_fiml'}
