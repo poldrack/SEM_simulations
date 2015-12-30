b3d7f0c5df91b52bd3f6b6ff03435b5c820e5fcc
poldrack <poldrack@gmail.com>
2015-12-29 20:22:36 -0800

    added timing sim

--- formsim.py
+++ formsim.py
@@ -1,6 +1,8 @@
 ### simulate form-based sampling
 
 import numpy
+import itertools
+import matplotlib.pyplot as plt
 
 nvars=70
 nforms=10
@@ -9,6 +11,15 @@ nformspersubject=2
 
 target_N=200
 formreps=200
+
+# simulate times for each measure, following a truncated exponential
+def mk_vartimes(min_time=2,max_time=20,mean_time=5,time_tolerance=1):
+    vartimes=numpy.zeros(nvars)
+    while numpy.abs(numpy.mean(vartimes)-mean_time)>time_tolerance:
+        vartimes=numpy.random.exponential(numpy.ones(nvars)*(mean_time))
+        vartimes[vartimes<min_time]=min_time
+        vartimes[vartimes>max_time]=max_time
+    return vartimes
 
 def make_random_forms(nvars,nforms,shuffle_forms=True):
     nvarsperform=nvars/nforms
@@ -20,20 +31,54 @@ def make_random_forms(nvars,nforms,shuffle_forms=True):
     if shuffle_forms:
         numpy.random.shuffle(v)
     return v
+
+def makeformlist(nvars,nforms):
+    nvarsperform=nvars/nforms
+    v=[]
+    for j in range(nvarsperform):
+        for i in range(nforms):
+            v.append(i)
+    v=numpy.array(v)
+    return v
     
-nsets=target_N/Nperform
-
-
+def make_random_forms_timed(nvars,nforms,vartimes):
+    assert len(vartimes)==nvars
+    nvarsperform=nvars/nforms
+    v=makeformlist(nvars,nforms)
+    formtime=numpy.zeros(nforms)
+    formtime[0]=100000
+    formtime_tolerance=10
+      
+    # sort vartimes and then randomize within form
+    while (numpy.max(formtime) - numpy.min(formtime))>formtime_tolerance:
+        vt_idx=numpy.argsort(vartimes)
+        
+        vt_idx_shuffled=vt_idx.copy()
+        
+        for f in range(nforms):
+            start_idx=f*nvarsperform
+            end_idx=(f+1)*nvarsperform
+            tmp=vt_idx[start_idx:end_idx].copy()
+            numpy.random.shuffle(tmp)
+            vt_idx_shuffled[start_idx:end_idx]=tmp
+        vs=v[vt_idx_shuffled]
+        formtime=numpy.zeros(nforms)
+        for f in range(nforms):
+            formtime[f]=numpy.sum(vartimes[vs==f])
+        #print (numpy.max(formtime) - numpy.min(formtime))
+    return vs,formtime
 
 def sample_forms(nvars,nforms,nformspersubject,formreps=1,shuffle_forms=True):
     covmatrix=numpy.zeros((nvars,nvars))
     covmatrix_triu=numpy.triu_indices(covmatrix.shape[0],1)
-
+    vartimes=mk_vartimes()
+    
     subs=0
+    setnum=1
     #while subs < target_N*choose(nforms,nformspersubject):
     while numpy.min(covmatrix[covmatrix_triu])<target_N:
-
-        forms=make_random_forms(nvars,nforms,shuffle_forms=shuffle_forms)
+        print 'set %d'%setnum
+        forms,formtimes=make_random_forms_timed(nvars,nforms,vartimes)
         for formrep in range(formreps):
             for sub_forms in itertools.combinations(range(nforms),nformspersubject):
                 for i in range(len(sub_forms)):
@@ -48,9 +93,11 @@ def sample_forms(nvars,nforms,nformspersubject,formreps=1,shuffle_forms=True):
                                 for jt in range(it,len(alltasks)):
                                     if it!=jt:
                                         covmatrix[alltasks[it],alltasks[jt]] +=1
+        setnum+=1
     return covmatrix
 
 covmat_shuf=sample_forms(nvars,nforms,nformspersubject,formreps=1,shuffle_forms=True)
+covmatrix_triu=numpy.triu_indices(covmat_shuf.shape[0],1)
 covmat_noshuf=sample_forms(nvars,nforms,nformspersubject,formreps=formreps,shuffle_forms=True)
 plt.figure(figsize=(10,10))
 plt.subplot(2,2,1)
@@ -71,4 +118,4 @@ plt.subplot(2,2,4)
 plt.hist(covmat_noshuf[covmatrix_triu],100)
 plt.title('pairwise sampling frequency')
 
-plt.savefig('formsims.pdf')
+plt.savefig('formsims_timed.pdf')
